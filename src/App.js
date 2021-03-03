d33fc2580c24c4ececc0d8f5becbfd1c48bf9100
William Bryan <wwbryan00@gmail.com>
2021-03-03 01:14:11 -0800

    add navbar and nomatch route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
 
       <Switch>
         <Route exact path={["/", "/home"]} component={Home} />
-        <Route exact path={["/projects"]} component={Projects} />
-        <Route exact path={["/Contact"]} component={Contact} />
+        <Route exact path={"/projects"} component={Projects} />
+        <Route exact path={"/Contact"} component={Contact} />
+        <Route component={ NoMatch } />
       </Switch>
     </Router>
   );
---
+++ src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import "./navStyle.css";
+import React from "react";
+
+function Nav() {
+    <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="/">William Bryan</a>
+
+            {/* Div containing social/resume icons */}
+            <div className="me-auto">
+                <i className="far fa-file"></i>
+                <i className="fab fa-github"></i>
+                <i className="fab fa-linkedin-in"></i>
+            </div>
+        </div>
+    </nav>
+}
+
+export default Nav;
