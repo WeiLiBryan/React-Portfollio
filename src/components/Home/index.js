e71fc25005c95361ecf6ae55dd195708eff6e83b
William Bryan <wwbryan00@gmail.com>
2021-03-03 01:25:39 -0800

    add hero component and link it to home

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import "./homeStyle.css";
+import React from "react";
+import Hero from "../Hero";
+
+function Home() {
+    return (
+        <div className="container">
+            <Hero />
+        </div>
+
+    );
+}
+
+export default Home;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,18 +2,20 @@ import "./navStyle.css";
 import React from "react";
 
 function Nav() {
-    <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/">William Bryan</a>
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">William Bryan</a>
 
-            {/* Div containing social/resume icons */}
-            <div className="me-auto">
-                <i className="far fa-file"></i>
-                <i className="fab fa-github"></i>
-                <i className="fab fa-linkedin-in"></i>
+                {/* Div containing social/resume icons */}
+                <div className="me-auto">
+                    <i className="far fa-file"></i>
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-linkedin-in"></i>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    );
 }
 
 export default Nav;
