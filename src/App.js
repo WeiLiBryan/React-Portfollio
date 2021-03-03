f652e4616b712dd594a32e4b66d114f67c11695c
William Bryan <wwbryan00@gmail.com>
2021-03-03 00:56:43 -0800

    add react routes to /home, /projects and /contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* Navigation Bar */}
+      <Nav />
+
+      <Switch>
+        <Route exact path={["/", "/home"]} component={Home} />
+        <Route exact path={["/projects"]} component={Projects} />
+        <Route exact path={["/Contact"]} component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
