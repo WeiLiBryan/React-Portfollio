5e819635b6a26e184c1d4fc23187407efcc0e119
William Bryan <wwbryan00@gmail.com>
2021-03-03 02:12:38 -0800

    add navbar styling and fix links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import NoMatch from "./components/NoMatch";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
 
       <Switch>
         <Route exact path={["/", "/home"]} component={Home} />
-        <Route exact path={"/projects"} component={Projects} />
-        <Route exact path={"/Contact"} component={Contact} />
-        <Route component={ NoMatch } />
+        {/* <Route exact path={"/projects"} component={Projects} />
+        <Route exact path={"/Contact"} component={Contact} /> */}
+        {/* <Route component={ NoMatch } /> */}
       </Switch>
     </Router>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,15 +3,21 @@ import React from "react";
 
 function Nav() {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar bg-transparent">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">William Bryan</a>
 
                 {/* Div containing social/resume icons */}
-                <div className="me-auto">
-                    <i className="far fa-file"></i>
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-linkedin-in"></i>
+                <div className="social">
+                    
+                    {/* Resume */}
+                    <a href="https://drive.google.com/file/d/1us60yQRFY5PqgJMXwf9scMxRK7DQh_sM/view?usp=sharing"><i class="fas fa-file"></i></a>
+
+                    {/* Github */}
+                    <a href="https://github.com/WeiLiBryan"><i class="fab fa-github"></i></a>
+
+                    {/* LinkedIn */}
+                    <a href="https://www.linkedin.com/in/william-bryan-72730019a/"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </nav>
