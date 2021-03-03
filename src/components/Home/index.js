45ec3cc5fa17b7085478ed053d08730c941ac48d
William Bryan <wwbryan00@gmail.com>
2021-03-03 01:46:24 -0800

    move hero to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,15 @@
 import "./homeStyle.css";
 import React from "react";
-import Hero from "../Hero";
 
 function Home() {
     return (
-        <div className="container">
-            <Hero />
+        <div className="bg-light p-5 rounded-lg m-3">
+            <h1 className="display-4">Hello, world!</h1>
+            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr className="my-4" />
+            <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </div>
-
     );
 }
 
