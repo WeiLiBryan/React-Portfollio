e509d47a1657c5bbdad5ff4077ecbd78f7cdcf5b
William Bryan <wwbryan00@gmail.com>
2021-03-03 06:00:16 -0800

    fix images not displaying

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,53 +7,53 @@ function Projects() {
         <div className="proj-content mx-grid">
             <h1 className="display-4">Projects</h1> <br />
 
-            <div class="card">
-                <img src="/assets/Clicky.png" class="card-img-top" alt="clicky" />
-                <div class="card-body">
+            <div className="card">
+                <img src={process.env.PUBLIC_URL + "/assets/Clicky.png"} className="card-img-top" alt="clicky" />
+                <div className="card-body">
                     <h6>Clicky: Memory Game</h6>
-                    <a class="btn btn-primary github" href="https://github.com/WeiLiBryan/Clicky" role="button">
-                        <i class="fab fa-github"></i>
+                    <a className="btn btn-primary github" href="https://github.com/WeiLiBryan/Clicky" role="button">
+                        <i className="fab fa-github"></i>
                     </a>
-                    <a class="btn btn-primary github" href="https://weilibryan.github.io/Clicky/" role="button">
+                    <a className="btn btn-primary github" href="https://weilibryan.github.io/Clicky/" role="button">
                         <i>Site</i>
                     </a>
                 </div>
             </div>
 
-            <div class="card">
-                <img src="/assets/love-connect.png" class="card-img-top" alt="love-connect" />
-                <div class="card-body">
+            <div className="card">
+                <img src={process.env.PUBLIC_URL + "/assets/love-connect.png"} className="card-img-top" alt="love-connect" />
+                <div className="card-body">
                     <h6>Love-Connect: Dating App</h6>
-                    <a class="btn btn-primary github" href="https://github.com/WeiLiBryan/Love-Connect" role="button">
-                        <i class="fab fa-github"></i>
+                    <a className="btn btn-primary github" href="https://github.com/WeiLiBryan/Love-Connect" role="button">
+                        <i className="fab fa-github"></i>
                     </a>
-                    <a class="btn btn-primary github" href="https://loveconnect14.herokuapp.com/" role="button">
+                    <a className="btn btn-primary github" href="https://loveconnect14.herokuapp.com/" role="button">
                         <i>Site</i>
                     </a>
                 </div>
             </div>
 
-            <div class="card">
-                <img src="/assets/Note-Taker.png" class="card-img-top" alt="note-taker" />
-                <div class="card-body">
+            <div className="card">
+                <img src={process.env.PUBLIC_URL + "/assets/Note-Taker.png"} className="card-img-top" alt="note-taker" />
+                <div className="card-body">
                     <h6>Note-Taker</h6>
-                    <a class="btn btn-primary github" href="https://github.com/WeiLiBryan/Note-Taker" role="button">
-                        <i class="fab fa-github"></i>
+                    <a className="btn btn-primary github" href="https://github.com/WeiLiBryan/Note-Taker" role="button">
+                        <i className="fab fa-github"></i>
                     </a>
-                    <a class="btn btn-primary github" href="https://polar-taiga-31548.herokuapp.com/" role="button">
+                    <a className="btn btn-primary github" href="https://polar-taiga-31548.herokuapp.com/" role="button">
                         <i>Site</i>
                     </a>
                 </div>
             </div>
 
-            <div class="card">
-                <img src="/assets/Unwind.png" class="card-img-top" alt="unwind" />
-                <div class="card-body">
+            <div className="card">
+                <img src={process.env.PUBLIC_URL + "/assets/Unwind.png"} className="card-img-top" alt="unwind" />
+                <div className="card-body">
                     <h6>Unwind: Relaxation App</h6>
-                    <a class="btn btn-primary github" href="https://github.com/WeiLiBryan/Unwind" role="button">
-                        <i class="fab fa-github"></i>
+                    <a className="btn btn-primary github" href="https://github.com/WeiLiBryan/Unwind" role="button">
+                        <i className="fab fa-github"></i>
                     </a>
-                    <a class="btn btn-primary github" href="https://weilibryan.github.io/Unwind/" role="button">
+                    <a className="btn btn-primary github" href="https://weilibryan.github.io/Unwind/" role="button">
                         <i>Site</i>
                     </a>
                 </div>
